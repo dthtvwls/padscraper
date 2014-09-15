d9a576024a5e254c1ba2209debea11dc9aaf76a4
Joshua Stauter <joshua@localhost.local>
2014-09-14 21:12:09 -0400

    add geocoding to scrape, but not in parallel (how to pass neighborhood?)

--- src/Scrape.php
+++ src/Scrape.php
@@ -4,9 +4,9 @@
  */
 class Scrape {
   function perform() {
-    $q  = DB::query('SELECT link FROM listings WHERE scraped != TRUE', PDO::FETCH_ASSOC);
+    $q  = DB::query('SELECT link, neighborhood FROM listings WHERE scraped != TRUE', PDO::FETCH_ASSOC);
     $ps = DB::prepare('UPDATE listings SET scraped=TRUE, street=:street, description=:description, lat=:lat, lng=:lng WHERE link=:link');
-
+    /*
     Guzzle::sendAll(array_map(function ($listing) {
       return Guzzle::createRequest('GET', 'http://newyork.craigslist.org' . $listing['link']);
     }, iterator_to_array($q)), ['complete' => function ($event) use($ps) {
@@ -29,7 +29,7 @@ class Scrape {
         Logger::error($e->getMessage(), $ps->errorinfo());
       }
     }]);
-    /*
+    */
     foreach ($q as $listing) {
       try {
         $body = Guzzle::get('http://newyork.craigslist.org' . $listing['link'])->getBody();
@@ -39,10 +39,14 @@ class Scrape {
 
         $street = $crawler->filter('.mapAndAttrs > .mapbox > div.mapaddress');
         
+        $url  = 'http://maps.googleapis.com/maps/api/geocode/json?address=' . ($street->count() ? $street->text() : $listing['neighborhood']);
+        $json = json_decode(Guzzle::get($url)->getBody(), true);
+        $loc  = isset($json['results'][0]) ? $json['results'][0]['geometry']['location'] : null;
+        
         $ps->execute([
           ':link' => $listing['link'],
-          ':lat'  => null,
-          ':lng'  => null,
+          ':lat'  => isset($loc['lat']) ? $loc['lat'] : null,
+          ':lng'  => isset($loc['lng']) ? $loc['lng'] : null,
           ':street' => $street->count() ? $street->text() : null,
           ':description' => $readability->init() ? trim(strip_tags(tidy_parse_string($readability->getContent()->innerHTML, [], 'UTF8'))) : null    
         ]);
@@ -50,6 +54,6 @@ class Scrape {
         Logger::error($e->getMessage(), $ps->errorinfo());
       }
     }
-    */
+    
   }
 }
